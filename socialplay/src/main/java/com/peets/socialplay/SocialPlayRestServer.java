d052db238600af1294c85e14ae19b447cb3cdf86
cmti95035 <cmti95035@gmail.com>
2015-04-19 13:02:38 -0700

    Add SocialPlayRestServer.

---
+++ socialplay/src/main/java/com/peets/socialplay/SocialPlayRestServer.java
@@ -0,0 +1,207 @@
+package com.peets.socialplay;
+
+import com.linkedin.r2.RemoteInvocationException;
+import com.linkedin.r2.transport.common.Client;
+import com.linkedin.r2.transport.common.bridge.client.TransportClientAdapter;
+import com.linkedin.r2.transport.http.client.HttpClientFactory;
+import com.linkedin.restli.client.ActionRequest;
+import com.linkedin.restli.client.CreateRequest;
+import com.linkedin.restli.client.Response;
+import com.linkedin.restli.client.ResponseFuture;
+import com.linkedin.restli.client.RestClient;
+import com.linkedin.restli.common.EmptyRecord;
+import com.peets.socialplay.server.Account;
+import com.peets.socialplay.server.ActivationBuilders;
+import com.peets.socialplay.server.Identity;
+import com.peets.socialplay.server.IdentityType;
+import com.peets.socialplay.server.RegistrationBuilders;
+import com.peets.socialplay.server.RegistrationCreateBuilder;
+import com.peets.socialplay.server.RegistrationDoInviteBuilder;
+import com.peets.socialplay.server.RegistrationDoKeepLiveBuilder;
+import com.peets.socialplay.server.RegistrationDoRegisterAccountBuilder;
+import com.peets.socialplay.server.SocialPlayBuilders;
+import com.peets.socialplay.server.SocialPlayContext;
+import com.peets.socialplay.server.SocialPlayCreateBuilder;
+import com.peets.socialplay.server.SocialPlayDoFindIncomingInvitationBuilder;
+import com.peets.socialplay.server.SocialPlayDoFindParticipantJoinedBuilder;
+import com.peets.socialplay.server.SocialPlayDoInviteToChatBuilder;
+import com.peets.socialplay.server.SocialPlayDoUpdateParticipantJoinedBuilder;
+
+import java.util.Collections;
+
+import android.util.Log;
+
+public class SocialPlayRestServer {
+    // Create an HttpClient and wrap it in an abstraction layer
+    private static final HttpClientFactory http = new HttpClientFactory();
+    private static final Client r2Client = new TransportClientAdapter(
+            http.getClient(Collections.<String, String> emptyMap()));
+    private static final String BASE_URL = "http://54.183.228.194:8080/SocialPlay-server/";
+    // Create a RestClient to talk to localhost:8080
+    private static RestClient restClient = new RestClient(r2Client, BASE_URL);
+    private static RegistrationBuilders registrationBuilders = new RegistrationBuilders();
+    private static SocialPlayBuilders socialPlayBuilders = new SocialPlayBuilders();
+    private static ActivationBuilders activationBuilders = new ActivationBuilders();
+    private static final String TAG = "SocialPlayRestServer";
+
+    /**
+     * ping to server to keep live
+     * @param accountId
+     * @return
+     */
+    public static Boolean keepLive(long accountId){
+        try {
+            RegistrationDoKeepLiveBuilder registrationDoKeepLiveBuilder = registrationBuilders.actionKeepLive();
+
+            ActionRequest<Boolean> keepAliveRequest = registrationDoKeepLiveBuilder.paramAccountId(accountId).build();
+            ResponseFuture<Boolean> keepAliveFuture = restClient.sendRequest(keepAliveRequest);
+            Response<Boolean> keepAliveResponse = keepAliveFuture.getResponse();
+
+            return keepAliveResponse.getEntity();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing keep live: " + ex.getMessage());
+        }
+
+        return false;
+    }
+
+    /**
+     * invite a friend -- this is different from chat invitation
+     * @param invitor
+     * @param invitee
+     * @return
+     */
+    public static Boolean invite(long invitor, long invitee)
+    {
+        try {
+            RegistrationDoInviteBuilder registrationDoKeepLiveBuilder = registrationBuilders.actionInvite();
+
+            ActionRequest<Boolean> inviteRequest = registrationDoKeepLiveBuilder.paramInvitor(invitor).paramInvitee(invitee).build();
+            ResponseFuture<Boolean> inviteFuture = restClient.sendRequest(inviteRequest);
+            Response<Boolean> inviteFutureResponse = inviteFuture.getResponse();
+
+            return inviteFutureResponse.getEntity();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing keep live: " + ex.getMessage());
+        }
+
+        return false;
+    }
+
+    /**
+     * invite a friend to a remote play
+     * @param invitor
+     * @param invitee
+     * @return video chat Room ID
+     */
+    public static String inviteToChat(long invitor, long invitee)
+    {
+        try{
+            SocialPlayDoInviteToChatBuilder socialPlayDoInviteToChatBuilder = socialPlayBuilders.actionInviteToChat();
+            ActionRequest<String> actionRequest = socialPlayDoInviteToChatBuilder.paramInvitor(invitor).paramInvitee(invitee).build();
+            ResponseFuture<String> actionFuture = restClient.sendRequest(actionRequest);
+            Response<String> actionResponse = actionFuture.getResponse();
+
+            return actionResponse.getEntity();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing inviteToChat: " + ex.getMessage());
+        }
+
+        return null;
+    }
+
+    /**
+     * check whether the invited participant joined the chat
+     * @param invitor
+     * @param invitee
+     * @param roomId
+     * @return
+     */
+    public static Boolean findParticipantJoined(long invitor, long invitee, String roomId)
+    {
+        try{
+            SocialPlayDoFindParticipantJoinedBuilder socialPlayDoFindParticipantJoinedBuilder = socialPlayBuilders.actionFindParticipantJoined();
+            ActionRequest<Boolean> actionRequest = socialPlayDoFindParticipantJoinedBuilder.paramInvitor(invitor).paramInvitee(invitee).paramRoomId(roomId).build();
+            ResponseFuture<Boolean> actionFuture = restClient.sendRequest(actionRequest);
+            Response<Boolean> actionResponse = actionFuture.getResponse();
+
+            return actionResponse.getEntity();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing findParticipantJoined: " + ex.getMessage());
+        }
+
+        return false;
+    }
+
+    /**
+     * find out whether there's any incoming invitation
+     * @param accountId
+     * @return
+     */
+    public static SocialPlayContext findIncomingInvitation(long accountId)
+    {
+        try{
+            SocialPlayDoFindIncomingInvitationBuilder socialPlayDoFindIncomingInvitationBuilder = socialPlayBuilders.actionFindIncomingInvitation();
+            ActionRequest<SocialPlayContext> actionRequest = socialPlayDoFindIncomingInvitationBuilder.paramInvitee(accountId).build();
+            ResponseFuture<SocialPlayContext> actionFuture = restClient.sendRequest(actionRequest);
+            Response<SocialPlayContext> actionResponse = actionFuture.getResponse();
+
+            return actionResponse.getEntity();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing findIncomingInvitation: " + ex.getMessage());
+        }
+
+        return null;
+    }
+
+    /**
+     * update the join status
+     * @param invitor
+     * @param invitee
+     * @param roomId
+     * @param joined
+     * @return
+     */
+    public static Boolean updateParticipantJoined(long invitor, long invitee, String roomId, Boolean joined)
+    {
+        try{
+            SocialPlayDoUpdateParticipantJoinedBuilder socialPlayDoUpdateParticipantJoinedBuilder = socialPlayBuilders.actionUpdateParticipantJoined();
+            ActionRequest<Boolean> actionRequest = socialPlayDoUpdateParticipantJoinedBuilder.paramInvitor(invitor).paramInvitee(invitee).paramRoomId(roomId).paramJoined(joined).build();
+            ResponseFuture<Boolean> actionFuture = restClient.sendRequest(actionRequest);
+            Response<Boolean> actionResponse = actionFuture.getResponse();
+
+            return actionResponse.getEntity();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing updateParticipantJoined: " + ex.getMessage());
+        }
+
+        return false;
+    }
+
+    public static Long registerAccount(IdentityType identityType, String identityStr, String userName)
+    {
+        try
+        {
+            RegistrationDoRegisterAccountBuilder registrationDoRegisterAccountBuilder = registrationBuilders.actionRegisterAccount();
+            Account account = new Account();
+            account.setIdentity(new Identity().setIdentityStr(identityStr).setIdentityType(identityType)).setName(userName);
+
+            ActionRequest<Account> actionRequest = registrationDoRegisterAccountBuilder.paramEntity(account).build();
+            ResponseFuture<Account> accountResponseFuture = restClient.sendRequest(actionRequest);
+            Response<Account> accountResponse = accountResponseFuture.getResponse();
+
+            return accountResponse.getEntity().getAccountId();
+        }catch (RemoteInvocationException ex)
+        {
+            Log.e(TAG, "Encountered error doing registerAccount: " + ex.getMessage());
+        }
+
+        return null;
+    }
+}
---
+++ socialplay/src/main/java/com/peets/socialplay/TreasureHuntRestActivity.java
@@ -0,0 +1,496 @@
+package com.peets.socialplay;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.peets.socialplay.server.SocialPlayContext;
+
+/*
+ * the main activity to host the webRTC connectivity
+ */
+public class TreasureHuntRestActivity extends Activity {
+    private static final String TAG = "TreasureHuntRestActivity";
+    private Button button1 = null;
+    private Button button2 = null;
+    private Button button3 = null;
+    private ImageView imageView = null;
+
+    // state information
+    private String chatRoom = null;
+    private boolean chatInProgress = false;
+
+    private MediaPlayer mp = null;
+    private MediaPlayer mp2 = null;
+    private PlayRingtoneTask pTask = null;
+    private long duration = 0;
+    private CheckExistingConnectionTask checkTask = null;
+    private static KeepLiveTask keepLiveTask = null;
+
+    public static String CHATROOM = "chatRoom";
+
+    public static String ACCOUNTID = "accountid";
+    private static Long myAccount = null;
+    private Long participantAccount = 1234568L;
+    /**
+     * Called when the activity is first created. This is where we'll hook up
+     * our views in XML layout files to our application.
+     **/
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        disableStrictMode();
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        myAccount = intent.getLongExtra(ACCOUNTID, 1234568L);
+
+        Log.e(TAG, "On create");
+        setContentView(R.layout.playdate);
+
+        if(keepLiveTask == null)
+            keepLiveTask = new KeepLiveTask();
+
+        keepLiveTask.execute((Void)null);
+
+        // this is the button for a user to connect to a friend
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "OnClick button1");
+
+                button1.setText(R.string.connecting);
+                disableButtons();
+                inviteToPlay();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "button2 OnClick");
+
+                button1.setText(R.string.connecting);
+                disableButtons();
+                inviteToPlay();
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "button2 OnClick");
+
+                button3.setText(R.string.connecting);
+                disableButtons();
+                inviteToPlay();
+            }
+        });
+
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        imageView.setImageResource(R.drawable.invite);
+        imageView.setVisibility(View.VISIBLE);
+    }
+
+    private void disableStrictMode() {
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+    }
+
+    private void disableButtons()
+    {
+        button1.setEnabled(false);
+        button2.setEnabled(false);
+        button3.setEnabled(false);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * Called when the activity is coming to the foreground. This is where we
+     * will check whether there's an incoming connection.
+     **/
+    @Override
+    protected void onStart() {
+        chatInProgress = false;
+        Log.e(TAG, "onStart");
+        super.onStart();
+
+        if(mp2 == null)
+        {
+            mp2 = MediaPlayer.create(this, R.raw.ringtone);
+        }
+        if(mp == null)
+        {
+            mp = MediaPlayer.create(this, R.raw.playdate);
+        }
+        duration = (long)mp.getDuration() + 500;
+        mp.start();
+
+        button1.setText(R.string.button_daniel);
+        button1.setEnabled(true);
+
+        button2.setText(R.string.button_charlie);
+        button2.setEnabled(true);
+
+        button3.setText(R.string.button_anne);
+        button3.setEnabled(true);
+    }
+
+    /**
+     * utility to do sleep
+     *
+     * @param milliseconds
+     */
+    public static void sleep(long milliseconds) {
+        try {
+            Log.e(TAG, "will sleep " + milliseconds
+                    + "milliseconds");
+            Thread.sleep(milliseconds);
+        } catch (Exception ex) {
+            Log.e(TAG,
+                    "sleep encounters exception: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Async task to constantly ping server to keep live
+     */
+    private class KeepLiveTask extends
+            AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            int count = 0;
+            while(count < 1) {
+                Boolean returnValue = SocialPlayRestServer.keepLive(myAccount);
+                Log.e(TAG, "keep live returns: " + returnValue);
+
+                // keep it constantly running in the background
+                sleep(30000);
+            }
+
+            return (Void) null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.e(TAG, "KeepLiveTask onPostExecute received: " + result);
+        }
+    }
+
+    /**
+     * Async task to initiate a remote play invitation
+     */
+    private class InviteToPlayTask extends
+            AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            String response = null;
+            int count = 0;
+            while (count < 3) {
+                Log.e(TAG, "InviteToPlayTask doInBackground chatInProgress: "
+                        + chatInProgress);
+
+                    response = SocialPlayRestServer.inviteToChat(myAccount, participantAccount);
+                    if(response != null)
+                        break;      // break infinite loop when seeing an incoming request
+                count++;
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            chatRoom = result;
+            Log.e(TAG, "InviteToPlayTask onPostExecute received: " + result);
+            Log.e(TAG, "will start CheckConnectionEstablishedTask");
+            CheckConnectionEstablishedTask checkConnectionEstablishedTask = new CheckConnectionEstablishedTask();
+            checkConnectionEstablishedTask.execute();
+        }
+    }
+
+    /**
+     * the Async task to constantly poll whether there's an incoming connection
+     *
+     */
+    private class CheckExistingConnectionTask extends
+            AsyncTask<Void, Void, SocialPlayContext> {
+        @Override
+        protected SocialPlayContext doInBackground(Void... params) {
+            SocialPlayContext response = null;
+            int count = 0;
+            while (count < 1) {
+                Log.e(TAG, "CheckExistingConnectionTask doInBackground chatInProgress: "
+                        + chatInProgress);
+
+                if (!chatInProgress) {
+                    response = SocialPlayRestServer.findIncomingInvitation(myAccount);
+                    if(response != null && response.hasChatRoomId())
+                        break;      // break infinite loop when seeing an incoming request
+                }
+                else
+                {
+                    Log.e(TAG, "CheckExistingConnectionTask will break because chatInProgress: "
+                            + chatInProgress);
+                    break;
+                }
+                sleep(1000);
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(SocialPlayContext result) {
+            chatRoom = result.getChatRoomId();
+            Log.e(TAG, "CheckExistingConnectionTask onPostExecute received: " + result);
+            Log.e(TAG, "will start PlayRingtoneTask");
+            pTask = new PlayRingtoneTask();
+            pTask.execute();
+            alertConnection("Your friend invites you to a play date",
+                    "Accept?");
+        }
+    }
+
+    /**
+     * the Async task to constantly poll whether an outgoing request is accepted
+     *
+     */
+    private class CheckConnectionEstablishedTask extends
+            AsyncTask<Boolean, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Boolean... params) {
+            Boolean response = false;
+            int count = 0;
+            while (count < 10) {
+                Log.e(TAG,
+                        "CheckConnectionEstablishedTask doInBackground count: "
+                                + count);
+
+                response = SocialPlayRestServer.findParticipantJoined(myAccount, myAccount, chatRoom);
+
+                if(response != null && response)
+                    return response;    // participant joined
+
+                sleep(1000);
+                count++;
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            Log.e(TAG, "onPostExecute result: "
+                    + result);
+            if(result)
+            {
+                // remote party accepted, now go to the video chat
+                proceedToChat();
+            }else{
+                Log.e(TAG, "onPostExecute show alert and back to start");
+                alert("Your friend didn't accept your invite", "Please try again!");
+                onStart();
+            }
+        }
+    }
+
+    /**
+     * Async task to update participant join status
+     */
+    private class UpdateParticipantJoinedTask extends
+            AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            Boolean response = false;
+            int count = 0;
+
+            // will try up to 5 times updating to the server
+            while (count < 5) {
+                Log.e(TAG,
+                        "updateParticipantJoinedTask doInBackground count: "
+                                + count);
+
+                response = SocialPlayRestServer.updateParticipantJoined(myAccount, myAccount, chatRoom,true);
+
+                if(response != null && response)
+                    return response;    // update successful
+
+                count++;
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            Log.e(TAG, "onPostExecute result: "
+                    + result);
+            if(result)
+            {
+                // remote party accepted, now go to the video chat
+                proceedToChat();
+            }else{
+                Log.e(TAG, "onPostExecute show alert and back to start");
+                alert("Your friend didn't accept your invite", "Please try again!");
+                onStart();
+            }
+        }
+    }
+
+    /**
+     * user clicks a button so initiate the task to invite a friend to play
+     */
+    private void inviteToPlay()
+    {
+        InviteToPlayTask inviteToPlayTask = new InviteToPlayTask();
+        inviteToPlayTask.execute();
+    }
+    /**
+     * both parties are ready, proceed to chat
+     */
+    private void proceedToChat()
+    {
+        imageView.setImageResource(R.drawable.treasurehunt);
+
+        Intent intent = new Intent(getApplicationContext(), TreasureHuntImageActivity.class);
+        intent.putExtra(CHATROOM, chatRoom);
+        if(mp != null)
+        {
+            mp.pause();
+        }
+        if(mp2 != null)
+        {
+            mp2.pause();
+        }
+        startActivity(intent);
+    }
+    /**
+     * the Async task to play ringtone upon an incoming connection
+     *
+     */
+    private class PlayRingtoneTask extends
+            AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... param) {
+            int count = 0;
+
+            while (count < 10) {
+                Log.e(TAG, "PlayRingtoneTask doInBackground count: "
+                        + count);
+
+                if (!chatInProgress) {
+                    Log.e(TAG, "PlayRingtoneTask doInBackground start playing ringtone");
+                    mp2.start();
+                }
+                else
+                {
+                    mp2.pause();
+                    break;
+                }
+                sleep(duration);
+                count++;
+            }
+
+            return (Void)null;
+        }
+
+    }
+
+    /**
+     * Show an alert dialog with message. user can either click OK or cancel no
+     * real action is triggered
+     *
+     * @param header
+     * @param message
+     */
+    private void alert(String header, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setTitle(header)
+                .setCancelable(true)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                dialog.dismiss();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /**
+     * pops up an alert dialog with 2 messages, one as the header and the other
+     * as the message to be shown in the dialog. if user clicks on NO, it enable
+     * the connect button on screen if user clicks on YES, it connects user to
+     * the incoming request
+     *
+     * @param header
+     * @param message
+     */
+    private void alertConnection(String header, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setTitle(header)
+                .setNegativeButton(R.string.no,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Log.e(TAG, "NO clicked, will stop mp2");
+                                stopRing();
+                                dialog.dismiss();
+                            }
+                        })
+                .setPositiveButton(android.R.string.yes,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Log.e(TAG, "OK clicked, will stop mp2");
+                                stopRing();
+                                dialog.dismiss();
+                                chatInProgress = true;
+
+                                // update the server that it accepted the request
+                                UpdateParticipantJoinedTask updateParticipantJoinedTask = new UpdateParticipantJoinedTask();
+                                updateParticipantJoinedTask.execute((Void)null);
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /**
+     * stop playing ringtone
+     */
+    private void stopRing()
+    {
+        if(mp2!=null){
+            mp2.pause();
+            chatInProgress = true;
+        }
+
+        if(pTask != null)
+        {
+            Log.e(TAG, "will cancel the pTask");
+            pTask.cancel(true);
+        }
+    }
+}
+
